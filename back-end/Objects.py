afa6c6950140d6b7d3405936850792db1d62f5d7
Raphael Augusto Teixeira Silva <raphaelaugustoteixeirasilva@gmail.com>
2020-08-15 13:16:59 -0300

    simplify definition of objects

--- back-end/Objects.py
+++ back-end/Objects.py
@@ -1,4 +1,5 @@
 from datetime import datetime as dt
+import MapObj as mp
 import abc
 
 class IObject(metaclass = abc.ABCMeta):
@@ -6,7 +7,7 @@ class IObject(metaclass = abc.ABCMeta):
     @abc.abstractstaticmethod
     def convert(obj):
         raise NotImplementedError
-        
+
 class Client(IObject):
     def __init__(self, _id, name, cpf, age, email, password, create_date = dt.now()):
         self._id = _id
@@ -16,23 +17,11 @@ class Client(IObject):
         self.email = email
         self.password = password
         self.create_date = create_date
-        
+
     @staticmethod
     def convert(obj):
-        if isinstance(obj, Client):
-            return obj
-        elif isinstance(obj, dict):
-            _id = obj.get("_id", None)
-            name = obj.get("name", None)
-            cpf = obj.get("cpf", None)
-            age = obj.get("age", None)
-            email = obj.get("email", None)
-            password = obj.get("password", None)
-            create_date = obj.get("create_date", None)
-            return Client(_id, name, cpf, age, email, password, create_date)        
-        else:
-            raise TypeError(f"Type '{obj.__class__.__name__}' must be a Dict or Client.")
-        
+        return mp.toClient(obj)
+    
 class Company(IObject):
     def __init__(self, _id, name, cnpj, email, password, create_date = dt.now()):
         self._id = _id
@@ -41,18 +30,7 @@ class Company(IObject):
         self.email = email
         self.password = password
         self.create_date = create_date
-        
+ 
     @staticmethod
     def convert(obj):
-        if isinstance(obj, Company):
-            return obj
-        elif isinstance(obj, dict):
-            _id = obj.get("_id", None)
-            name = obj.get("name", None)
-            cnpj = obj.get("cnpj", None)
-            email = obj.get("email", None)
-            password = obj.get("password", None)
-            create_date = obj.get("create_date", None)
-            return Company(_id, name, cnpj, email, password, create_date)        
-        else:
-            raise TypeError(f"Type '{obj.__class__.__name__}' must be a Dict or Client.")
+        return mp.toCompany(obj)   
