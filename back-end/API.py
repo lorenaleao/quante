5a02518c2003804187d88c9f6f62895f90da5cce
Raphael Augusto Teixeira Silva <raphaelaugustoteixeirasilva@gmail.com>
2020-08-15 01:02:16 -0300

    add log

--- back-end/API.py
+++ back-end/API.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
+import logging
 
 app = Flask(__name__)
+
+format = "LEVEL %(levelname)s: %(asctime)s\n%(message)s\n"
+logging.basicConfig(filename = "log/log_records.log", level = logging.ERROR, format = format)
 
 @app.route("/")
 def it_works():
