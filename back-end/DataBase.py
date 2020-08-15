4c29f5be625c38ddcbc409315b48246726f84543
Raphael Augusto Teixeira Silva <raphaelaugustoteixeirasilva@gmail.com>
2020-08-15 00:16:18 -0300

    added method of adding object to the bank

--- back-end/DataBase.py
+++ back-end/DataBase.py
@@ -13,3 +13,11 @@ class Collection():
             self.mongo_url = f"mongodb+srv://quante:{password}@db-quante.dni24.gcp.mongodb.net/{login}?retryWrites=true&w=majority"
             print(self.mongo_url)
             self._type = _type
+            
+    def add(self, obj):
+        del obj["_id"] 
+        with mg.MongoClient(self.mongo_url) as db_mongo:
+            collection = db_mongo["db-quante"][self._type.__name__]
+            ret = collection.insert_one(obj)
+            obj["_id"] = ObjectId(ret.inserted_id)
+            return obj
