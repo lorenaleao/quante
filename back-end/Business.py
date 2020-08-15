03f7f2d38329fc80ae1b075ab9a61a20a5f25566
Raphael Augusto Teixeira Silva <raphaelaugustoteixeirasilva@gmail.com>
2020-08-15 00:37:40 -0300

    added business client object

--- back-end/Business.py
+++ back-end/Business.py
@@ -5,3 +5,7 @@ class BusinessBase():
     def __init__(self, _type : IObject):
         self.collection = Collection(_type)
         self._type = _type
+        
+class ClientBusiness(BusinessBase):
+    def __init__(self):
+        super().__init__(Client)
