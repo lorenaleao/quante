1c25184e63406dd32ae7b9f2c844e0bfa1453e61
Raphael Augusto Teixeira Silva <raphaelaugustoteixeirasilva@gmail.com>
2020-08-15 00:20:54 -0300

    Adding object to represent the client

--- back-end/Objects.py
+++ back-end/Objects.py
@@ -1,3 +1,4 @@
+from datetime import datetime as dt
 import abc
 
 class IObject(metaclass = abc.ABCMeta):
@@ -5,3 +6,16 @@ class IObject(metaclass = abc.ABCMeta):
     @abc.abstractstaticmethod
     def convert(obj):
         raise NotImplementedError
+    
+class Client(IObject):
+    def __init__(self, name, age, _id = None, email = None, password = None, create_date = dt.now()):
+        self._id = _id
+        self.name = name
+        self.age = age
+        self.email = email
+        self.password = password
+        self.create_date = create_date
+        
+    @staticmethod
+    def convert(obj):
+        pass
