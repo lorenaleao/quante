20cd9bfeeeb336b8164ed33b4fd64ef770b54286
Raphael Augusto Teixeira Silva <raphaelaugustoteixeirasilva@gmail.com>
2020-08-29 15:53:39 -0300

    simplifying code

--- back-end/Objects.py
+++ back-end/Objects.py
@@ -1,5 +1,5 @@
+# Standart import
 from datetime import datetime as dt
-import Mapper as mp
 import abc
 
 class IObject(metaclass = abc.ABCMeta):
@@ -20,7 +20,19 @@ class Client(IObject):
         
     @staticmethod
     def convert(obj):
-        return mp.toClient(obj)
+        if isinstance(obj, Client):
+            return obj
+        elif isinstance(obj, dict):
+            _id = obj.get("_id", None)
+            name = obj.get("name", None)
+            cpf = obj.get("cpf", None)
+            age = obj.get("age", None)
+            email = obj.get("email", None)
+            password = obj.get("password", None)
+            create_date = obj.get("create_date", None)
+            return Client(_id, name, cpf, age, email, password, create_date)        
+        else:
+            raise TypeError("Type " + obj.__class__.__name__ + " must be a Dict or Client.")
     
 class Company(IObject):
     def __init__(self, _id, name, cnpj, email, password, create_date = dt.now()):
@@ -33,4 +45,16 @@ class Company(IObject):
 
     @staticmethod
     def convert(obj):
-        return mp.toCompany(obj)
+        if isinstance(obj, Company):
+            return obj
+        elif isinstance(obj, dict):
+            _id = obj.get("_id", None)
+            name = obj.get("name", None)
+            cnpj = obj.get("cnpj", None)
+            email = obj.get("email", None)
+            password = obj.get("password", None)
+            create_date = obj.get("create_date", None)
+            return Company(_id, name, cnpj, email, password, create_date)        
+        else:
+            raise TypeError(f"Type " + obj.__class__.__name__ + " must be a Dict or Company.")
+    
