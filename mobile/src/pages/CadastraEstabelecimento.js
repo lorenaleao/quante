f6274bcd95e6866385fa93b4278dec2d5d12ea01
guilhermealbm <gui.miranda.13@gmail.com>
2020-09-22 17:04:43 -0300

    add company communication

--- mobile/src/pages/CadastraEstabelecimento.js
+++ mobile/src/pages/CadastraEstabelecimento.js
@@ -1,11 +1,42 @@
 import React, { useState } from 'react';
-import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
-function CadastraEstabelecimento(){
+import {View, Text, TextInput, Button, StyleSheet, Alert} from 'react-native';
+
+import api from '../services/api';
+
+function CadastraEstabelecimento({navigation}){
 
     const [establishmentName, setEstablishmentName] = useState('');
+    const [cnpj, setCnpj] = useState('');
+    const [address, setAdress] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    function submitNewEstablishment() {
-        //const response = await api.post('', {params: {establishmentName}})
+    async function submitNewEstablishment() {
+        let today = new Date()
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+        let dateTime = date+' '+time
+        const response = await api.post('/company/post/', {
+            name: establishmentName,
+            cnpj,
+            address,
+            email,
+            password,
+            create_date: dateTime
+        }).then((response) => {
+            Alert.alert(
+                "Sucesso!",
+                "Estabelecimento cadastrado com sucesso!",
+                [
+                { text: "OK", onPress: () => navigation.navigate('Quanté?') }
+                ]
+            )
+            setEstablishmentName('')
+            setCnpj('')
+            setAdress('')
+            setEmail('')
+            setPassword('')
+        })
     }
 
     return(
@@ -21,6 +52,44 @@ function CadastraEstabelecimento(){
                 autoCorrect={false}
                 value={establishmentName}
                 onChangeText={setEstablishmentName}
+                />
+                <View style={{ height: 10 }}></View>
+                <TextInput 
+                style={styles.defaultTextInput}
+                placeholder="Insira o CNPJ do estabelecimento"
+                placeholderTextColor="#999"
+                autoCorrect={false}
+                value={cnpj}
+                onChangeText={setCnpj}
+                />
+                <View style={{ height: 10 }}></View>
+                <TextInput 
+                style={styles.defaultTextInput}
+                placeholder="Insira o endereço do estabelecimento"
+                placeholderTextColor="#999"
+                autoCapitalize="words"
+                autoCorrect={false}
+                value={address}
+                onChangeText={setAdress}
+                />
+                <View style={{ height: 10 }}></View>
+                <TextInput 
+                style={styles.defaultTextInput}
+                placeholder="Insira o email do estabelecimento"
+                placeholderTextColor="#999"
+                autoCorrect={false}
+                value={email}
+                onChangeText={setEmail}
+                />
+                <View style={{ height: 10 }}></View>
+                <TextInput 
+                style={styles.defaultTextInput}
+                placeholder="Insira a senha do estabelecimento"
+                placeholderTextColor="#999"
+                secureTextEntry={true}
+                autoCorrect={false}
+                value={password}
+                onChangeText={setPassword}
                 />
                 <View style={{ height: 10 }}></View>
                 <View style={{ height: 40 }}></View>
