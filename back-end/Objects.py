29c15dbc581ef00b3f172ad685d70c3867ecbb96
Raphael Augusto Teixeira Silva <raphaelaugustoteixeirasilva@gmail.com>
2020-09-22 00:12:11 -0300

    refactoring objects

--- back-end/Objects.py
+++ back-end/Objects.py
@@ -1,6 +1,5 @@
 # Standart import
 from typing import Union
-from datetime import datetime as dt
 
 class IObject():
     @staticmethod
@@ -8,7 +7,7 @@ class IObject():
         raise NotImplementedError
 
 class Client(IObject):
-    def __init__(self, _id, name, cpf, age, email, password, create_date = dt.now()):
+    def __init__(self, _id, name, cpf, age, email, password, create_date, perfil_img):
         self._id = _id
         self.name = name
         self.cpf = cpf
@@ -16,44 +15,50 @@ class Client(IObject):
         self.email = email
         self.password = password
         self.create_date = create_date
+        self.perfil_img = perfil_img
         
     @staticmethod
     def convert(obj : Union['Client', dict]) -> 'Client':
         if isinstance(obj, Client):
             return obj
         elif isinstance(obj, dict):
-            _id = obj.get("_id", None)
-            name = obj.get("name", None)
-            cpf = obj.get("cpf", None)
-            age = obj.get("age", None)
-            email = obj.get("email", None)
-            password = obj.get("password", None)
-            create_date = obj.get("create_date", None)
-            return Client(_id, name, cpf, age, email, password, create_date)        
+            return Client(
+                obj.get("_id", None), 
+                obj.get("name", None), 
+                obj.get("cpf", None), 
+                obj.get("age", None), 
+                obj.get("email", None), 
+                obj.get("password", None), 
+                obj.get("create_date", None), 
+                obj.get("perfil_img", None)
+            )        
         else:
             raise TypeError("Type " + obj.__class__.__name__ + " must be a Dict or Client.")
     
 class Company(IObject):
-    def __init__(self, _id, name, cnpj, email, password, create_date = dt.now()):
+    def __init__(self, _id, name, cnpj, email, password, create_date, address):
         self._id = _id
         self.name = name
         self.cnpj = cnpj
         self.email = email
         self.password = password
         self.create_date = create_date
+        self.address = address
 
     @staticmethod
     def convert(obj : Union['Company', dict]) -> 'Company':
         if isinstance(obj, Company):
             return obj
         elif isinstance(obj, dict):
-            _id = obj.get("_id", None)
-            name = obj.get("name", None)
-            cnpj = obj.get("cnpj", None)
-            email = obj.get("email", None)
-            password = obj.get("password", None)
-            create_date = obj.get("create_date", None)
-            return Company(_id, name, cnpj, email, password, create_date)        
+            return Company(
+                obj.get("_id", None), 
+                obj.get("name", None), 
+                obj.get("cnpj", None), 
+                obj.get("email", None), 
+                obj.get("password", None), 
+                obj.get("create_date", None), 
+                obj.get("address", None)
+            )         
         else:
             raise TypeError(f"Type " + obj.__class__.__name__ + " must be a Dict or Company.")
 
